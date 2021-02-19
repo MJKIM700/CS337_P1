83710de21b7ea6b0b81885cd53c5aee7212a4a81
alealeale10 <alejandrocardozo-mongelos2022@u.northwestern.edu>
2021-02-19 13:37:49 -0500

    works

--- findHost.py
+++ findHost.py
@@ -1,36 +1,25 @@
 from nltk.tag import pos_tag
 import pull_text
 import string
+import sys
 
-#tweets = pull_text.Data(year).tweets
-#tweets[index].text
-#for tweet in tweets:
-#    tweet.text
-
-spec_char = ['@', '#', '&']
 nono_words = ['golden', 'globe', 'globes', 'Golden', 'Globe', 'Globes', 'goldenglobes', 'GoldenGlobes','RT']
 
+def tweetData(year):
+    tweets = pull_text.Data(year).tweets
+    return tweets
 
-tweets = pull_text.Data(2015).tweets
+def processTweets(tweet_data):
+    hostTweetTexts = []
+    for tweet in tweet_data:
+        if "hosts" in tweet.text:
+            if ("should" in tweet.text) or ("next year" in tweet.text):
+                pass
+            else:
+                txt = tweet.text_unchanged.translate(str.maketrans('', '', string.punctuation))
+                hostTweetTexts.append(txt)
+    return hostTweetTexts
 
-hostTweetTexts = []
-for tweet in tweets:
-    if "hosts" in tweet.text:
-        if ("should" in tweet.text) or ("next year" in tweet.text):
-            pass
-        else:
-            txt = tweet.text_unchanged.translate(str.maketrans('', '', string.punctuation))
-            hostTweetTexts.append(txt)
-"""
-wfhost = []
-for tweet in tweets:
-    if "ferrell" in tweet.text and "hosts" in tweet.text: 
-        txt = tweet.text.translate(str.maketrans('', '', string.punctuation))
-        txt.replace("ferrell", "FERRELL")
-        txt.replace("host", "HOST")
-        wfhost.append(txt)
-print(wfhost)
-"""
 def rankProperNouns(lstOfSent):
     votes = {}
     # loop through each tweet in the list of tweets
@@ -58,20 +47,7 @@ def rankProperNouns(lstOfSent):
         properNouns = [word for word, pos in tokensWithTags if pos == 'NNP'] #makes a list of JUST WORDS, not POS
         for word in properNouns:
             if any(ele in word for ele in nono_words):
-                properNouns.remove(word)
-        #i = 0
-        #curr = properNouns[i]
-        #if len(properNouns) > 1:
-        #    while curr != properNouns[-1]:
-        #        new_curr = properNouns[i+1]
-        #        if any(ele in curr for ele in nono_words):
-        #            properNouns.remove(curr)
-        #            curr = new_curr
-        #        else:
-        #            i += 1
-        #            curr = properNouns[i]
-
-
+                properNouns.remove(word)       
         # if list of Proper Nouns is not empty:
         if properNouns != []:
             # loop through the list of Proper Nouns and store them in votes, updating their value if appropriate
@@ -98,9 +74,7 @@ def findHosts(Dict):
         shortlist.append(lstOfLst[i][0])
         shortlistwNum.append(lstOfLst[i])
         i += 1
-    print(shortlist)
     shortlistLen = [len(i.split()) for i in shortlist]
-    print(shortlistLen)
     single = []
     double = []
     for i in range(len(shortlist)):
@@ -108,82 +82,32 @@ def findHosts(Dict):
             single.append(shortlist[i])
         if shortlistLen[i] == 2:
             double.append(shortlist[i])
-    #print(single)
-    #print(double)
     potentialHost = []
     for i in range(len(single)):
         for j in range(len(single)):
-            #print('combo is: ' + str(single[i]) +' '+ str(single[j]))
             if str(single[i] +' '+ single[j]) in double:
                 firstLast = str(single[i] +' '+ single[j])
                 potentialHost.append(firstLast)
-                #print('inserting pair: ' + firstLast)
-    print('potentialHost is: ' + str(potentialHost))
     dictValues = []
     for i in range(len(potentialHost)):
         dictValues.append(Dict.get(potentialHost[i]))
-    print('dictValues is: '+str(dictValues))
     host1index = dictValues.index(max(dictValues))
     host1 = potentialHost[host1index]
     sortedValues = [val for val in dictValues]
     sortedValues.sort(reverse=True)
-    print('sortedValues is: ' + str(sortedValues))
-    print('dictValues after sort is: '+str(dictValues))
     host2value = sortedValues[1]
     host2index = dictValues.index(host2value)
     if dictValues[host2index] >= float(0.5 * Dict.get(host1)):
         host2 = potentialHost[host2index]
         print('Hosts are: {} and {}'.format(host1, host2))
+        return host1, host2
     else:
         print('Host is: {}'.format(host1))
+        return host1
     return
-"""
-    for i in shortlistCopy:
 
-            single.append(shortlist[i])
-        else:
-            double.append(shortlist[i])
-    print(single)
-    print(double)
-    return
-    """
-"""
-    potentialHosts = []
-    for i, j in single:
-        for k in double:
-            if single[i] in double[k] and single[j] in double[k]:
-                if double[k] not in potentialHosts:
-                    potentialHosts.append(double[k])
-                else: continue
-    return potentialHosts
-"""
-        #if i is a substring of another name and j is a substring of same name,
-            #select full name
-        #if i[0] is one word, put in one list: singlelst
-        #if i[0] is two words, put in another list: doublelst
-        #if i and j in singlelst are substrings of k in doublelst:
-            #possiblehost.append(doublelst[k])
-    
-    #print(shortList)
-
-
-#Tests
-"""
-string1 = "the host this year is Tina Fey"
-string2 = "I loved the host's jokes this year. Tina Fey is so funny."
-string3 = "Tina Fey did a great job hosting this year"
-string4 = "Fey hosts the golden globes for the first time"
-string5 = "the person hosting the golden globes this year will be Tina"
-string6 = "doesn't have anything about the h word"
-
-sents = [string1, string2, string3, string4, string5, string6]
-print(findProperNouns(sents))
-"""
-
-#print(rankProperNouns(hostTweetTexts))
-
-host_votes = rankProperNouns(hostTweetTexts)
-#print(sortDict(host_votes))
-print(findHosts(host_votes))
-
-
+if __name__ == '__main__':
+    rawData = tweetData(int(sys.argv[1]))
+    cleanData = processTweets(rawData)
+    host_votes = rankProperNouns(cleanData)
+    print(findHosts(host_votes))
