cf69da0fefde08007cb650db27eccf98d6ee6668
jsl8391 <59754753+jsl8391@users.noreply.github.com>
2021-02-19 18:48:44 -0600

    Messy award finder

--- awards.py
+++ awards.py
@@ -42,14 +42,20 @@ def ngrams_left(in_string, keywords):
     return ngram_list
 
 
-def ngrams_best(in_string):
+def ngrams_best(in_string, caps):
     ngram_list = []
+    capped = False
     if 'best' in in_string:
-        split_string = in_string.partition('best')
-        rest_string = split_string[1] + split_string[2]
-        rest_string = word_tokenize(rest_string)
-        for i in range(len(rest_string)):
-            ngram_list.append(rest_string[:i + 1])
+        split_string = list(in_string.partition('best'))
+        for cap in caps:
+            if cap in split_string[2]:
+                capped = True
+                split_string[2] = split_string[2].partition(cap)[0]
+        if capped:
+            rest_string = split_string[1] + split_string[2]
+            rest_string = word_tokenize(rest_string)
+            for i in range(len(rest_string)):
+                ngram_list.append(rest_string[:i + 1])
     return ngram_list
 
 
@@ -62,12 +68,12 @@ def valid_award(candidate, ignore_list):
     for iw in ignore_list:
         if iw in cand_lower:
             return False
-    if len(candidate) < 2:
+    if len(candidate) < 3:
         return False
-    # for punct in string.punctuation.replace('-', '').replace(',', ''):
-    #     for c in candidate:
-    #         if punct in c:
-    #             return False
+    for punct in string.punctuation.replace('-', '').replace(',', ''):
+        for c in candidate:
+            if punct in c:
+                return False
     tagged = pos_tag(candidate)
     if 'N' not in tagged[-1][1]:
         return False
@@ -75,6 +81,11 @@ def valid_award(candidate, ignore_list):
         return False
 
     return True
+
+
+def prune_string(str):
+    pruned_str = str
+    return pruned_str
 
 
 # get input data somehow
@@ -89,8 +100,9 @@ def valid_award(candidate, ignore_list):
 kw_right = ['wins', 'gets', 'won', 'for winning', 'win', 'presenting', 'get', ':', '-', 'goldenglobe for', 'wins for',
             '#goldenglobe', 'award for']
 kw_left = ['goes to', ':', '-', 'award', 'goldenglobe', 'golden globe', '#goldenglobe']
-ignore = ['who', 'show', 'host', 'for', 'golden', 'globe', 'goldenglobe', 'goldenglobes']
-cap_words = ['#', ':', 'for', '!', "'", 'http', 'win', 'at']
+ignore = ['who', 'show', 'host', 'for', 'golden', 'globe', 'goldenglobe', 'goldenglobes', 'of', 'dressed', 'fucking',
+          "'", 'part', 'red']
+cap_words = ['#', ':', 'for', '!', "'", 'http', 'win', 'at', 'nominee', 'award', 'goes', 'is', ',', '-', 'or']
 
 
 def get_awards(tt):
@@ -99,23 +111,24 @@ def get_awards(tt):
     for t in tt:
         # ngrams += ngrams_right(t, kw_right)
         # ngrams += ngrams_left(t, kw_left)
-        ngrams += ngrams_best(t)
+        ngrams += ngrams_best(t, cap_words)
     # print(ngrams)
     for ng in ngrams:
-        # ng = cand_trim(ng_old)
+        if valid_award(ng, ignore):
 
-        #  if valid_award(ng, ignore):
-
-        ng_str = ' '.join(ng)
-        if ng_str in list(votes):
-            votes[ng_str] += 1
-        else:
-            votes[ng_str] = 1
-
+            ng_str = ' '.join(ng)
+            # ng_str = prune_string(ng_str)
+            if ng_str in list(votes):
+                votes[ng_str] += 1
+            else:
+                votes[ng_str] = 1
+    num_keys = 0
     for key in list(votes):
-        if votes[key] > 10:
+        if votes[key] > 40:
+            num_keys += 1
             print(key + ":")
             print(votes[key])
+    print(num_keys)
 
 
 if __name__ == '__main__':
